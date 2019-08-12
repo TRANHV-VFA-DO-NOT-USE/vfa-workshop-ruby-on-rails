c8d3c7e4044fdf586223a4a222989be71dd6242b
TranHV <vfa.tranhv@gmail.com>
2019-08-13 00:03:53 +0700

    update test

--- courses/demo/test/controllers/factory_controller_test.rb
+++ courses/demo/test/controllers/factory_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class FactoryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create Factory" do
+    post "/factory", params: { factory: { name: 'New test factory' } }   
+    Factory.count.eql?(3)
+    assert_redirected_to factory_index_path
+  end
+  test "should create Factory failed" do
+    post "/factory", params: { factory: { concho: 'New test factory' } }   
+    Factory.count.eql?(2)
+  end
 end
